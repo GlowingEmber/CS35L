05e77394c8d47a887d5ba5b7391dbe9aae2ed4da
Victoriayu0827 <149911184+Victoriayu0827@users.noreply.github.com>
2023-12-03 15:51:28 -0800

    Added message

--- S2/models/index.js
+++ S2/models/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 const UserModel = require('./models/user')
+const messageModel = require('./models/message')
 
 const app = express()
 app.use(express.json())
