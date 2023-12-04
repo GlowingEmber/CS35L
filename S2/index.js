60c75879c74c67d01c6c8da044f89a9df54b12cd
Victoriayu0827 <149911184+Victoriayu0827@users.noreply.github.com>
2023-12-03 16:42:15 -0800

    removed send message and retrieve conversation

--- S2/index.js
+++ S2/index.js
@@ -9,26 +9,6 @@ app.use(cors())
 
 mongoose.connect("mongodb+srv://max09lui:T4XEs0OHoUJIcJGF@cluster0.kcdmgrl.mongodb.net/?retryWrites=true&w=majority")
 
-app.get('/getConversation/:userId1/:userId2', async (req, res) => {
-    const { userId1, userId2 } = req.params;
-    try {
-        const conversation = await getConversation(userId1, userId2);
-        res.status(200).json({ message: 'Conversation retrieved successfully', data: conversation });
-    } catch (error) {
-        res.status(500).json({ message: 'Error retrieving conversation', error: error.message });
-    }
-});
-
-app.post('/sendMessage', async (req, res) => {
-    const { senderId, receiverId, text } = req.body;
-    try {
-        const newMessage = await addMessage(senderId, receiverId, text);
-        res.status(200).json({ message: 'Message sent successfully', data: newMessage });
-    } catch (error) {
-        res.status(500).json({ message: 'Error sending message', error: error.message });
-    }
-});
-
 app.post("/login", (req, res) =>{ // API endpoint
     const {name, pw} = req.body;
     UserModel.findOne({name:name})
