6474548a38cb2ea8844789c2eefb4a1b1c6c07a1
Victoriayu0827 <149911184+Victoriayu0827@users.noreply.github.com>
2023-12-03 19:41:16 -0800

    Update sendmessage

--- S2/index.js
+++ S2/index.js
@@ -23,7 +23,7 @@ app.get('/getConversation/:userId1/:userId2', async (req, res) => {
 app.post('/sendMessage', async (req, res) => {
     const { senderId, receiverId, text } = req.body;
     try {
-        const newMessage = await addMessage(senderId, receiverId, text);
+        const newMessage = await messageModel.addMessage(senderId, receiverId, text);
         res.status(200).json({ message: 'Message sent successfully', data: newMessage });
     } catch (error) {
         res.status(500).json({ message: 'Error sending message', error: error.message });
