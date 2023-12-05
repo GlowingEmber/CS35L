ab0a7c4c3358fea9bcacc515609c3a2173b9451b
Ethan Hopkins <ethan.e.hopkins@gmail.com>
2023-12-05 12:28:20 -0800

    move

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 const UserModel = require('./models/user')
+const FriendReqModel = require('./models/friendreq')
 
 const app = express()
 app.use(express.json())
@@ -9,15 +10,12 @@ app.use(cors())
 
 mongoose.connect("mongodb+srv://max09lui:T4XEs0OHoUJIcJGF@cluster0.kcdmgrl.mongodb.net/?retryWrites=true&w=majority")
 
-
-
 app.post("/login", (req, res) =>{ // API endpoint
     const {name, pw} = req.body;
     UserModel.findOne({name:name})
     .then(user => {
         if(user) {
             if(user.pw === pw){
-                console.log(user)
                 res.json(
                     {status:"Success",
                     name:user.name,
@@ -35,7 +33,117 @@ app.post("/login", (req, res) =>{ // API endpoint
         }
     })
 })
+
+app.get('/checkFriends', (req, res) => {
+    try {
+        const person1 = req.query.person1;
+        const person2 = req.query.person2;
+        FriendReqModel.findOne({
+            $or: [
+                {friender: person1, recipient: person2, accepted: true},
+                {friender: person2, recipient: person1, accepted: true}
+            ]
+        })
+        .then(friendship => {
+            var result = false;
+            if (friendship) { result = true; }
+            res.json({results: result})
+        })
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+app.get('/getFriendsList/:user', async (req, res) => {
+    try {
+        const user = req.params.user;
+        FriendReqModel.find({
+            $or: [
+                {friender: user, accepted: true},
+                {recipient: user, accepted: true}
+            ]
+        })
+        .then(friendsList => {
+            res.json({friends: friendsList})
+        })
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+app.get('/getIncomingFriendRequests/:user', async (req, res) => {
+    try {
+        const user = req.params.user;
+        FriendReqModel.find({recipient: user,accepted: false})
+        .then(friendRequests => {
+            res.json({friendRequests: friendRequests})
+        })
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+app.get('/getOutgoingFriendRequests/:user', async (req, res) => {
+    try {
+        const user = req.params.user;
+        FriendReqModel.find({friender: user,accepted: false})
+        .then(friendRequests => {
+            res.json({friendRequests: friendRequests})
+        })
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+
+////////////////////////////
+//// WIP BELOW
+////////////////////////////
+
+  
+app.post('/sendFriendRequest', (req, res) => {
+    const {friender, recipient, timestamp} = req.body;
+    const time_sent = Date.now();
+    FriendReqModel.create({friender, recipient, timestamp:time_sent, accepted:false})
+            .then(fReq => res.json({
+                status: "Success",
+                friendRequest: fReq}))
+            .catch(err => res.json(err))
+    // const {friender, recipient, accepted, timestamp} = req.body;
+
+    /*
+    FriendReqModel.find({
+        $or: [
+            {$and: [{friender: req.params.friender},{recipient: req.params.recipient}]},
+            {$and: [{friender: req.params.recipient},{recipient: req.params.friender}]}
+        ]
+    })
+    .then(user => {
+        if(user) {
+            res.json({
+                status: "Failure",
+                user: null
+            })
+        } else{
+            UserModel.create({name,pw, color, count:0})
+            .then(user => res.json({
+                status: "Success",
+                user: user}))
+            .catch(err => res.json(err))
+        }
+    })
+    */
+})
+
 /*
+app.delete('/deleteFriendRequest/:user_id', async (req, res) => {
+  }); 
+*/
+
+///////////////////////////
+//// END OF WIP
+///////////////////////////
+
 app.post('/register', (req, res) =>{ // request, response
     const {name, pw, color} = req.body
 
@@ -66,7 +174,21 @@ app.get('/getUserData', async (req, res) => {
     } catch (error) {
       res.status(500).json(error);
     }
-  });
+});
+
+app.get('/getUserId', async (req, res) => {
+    try {
+        const { name } = req.query;
+        const query = name ? { name } : {};
+        
+        const user = await UserModel.findOne(query);
+        res.json({
+            id:user._id
+        });
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 app.put('/updateUserCount/:id', async (req, res) => {
     try {
@@ -109,7 +231,35 @@ app.delete('/deleteDocument/:id', async (req, res) => {
       res.status(500).json({ error: 'Server error' });
     }
   });
-*/
+
+
+  app.put('/updateUserBio/:id', async (req, res) => {
+    try {
+        const { id } = req.params; // Get the ID of the user to update from the URL parameter
+        const { bio } = req.body; // Get the new bio value from the request body
+
+        // Use Mongoose to update the bio field for the specified user
+        const updatedUser = await UserModel.findByIdAndUpdate(id, { $set: { bio } }, { new: true });
+
+        res.json(updatedUser);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+app.put('/updateProfilePicture/:id', async (req, res) => {
+    try {
+        const { id } = req.params; // Get the ID of the user to update from the URL parameter
+        const { profilepicture } = req.body; // Get the new bio value from the request body
+
+        // Use Mongoose to update the bio field for the specified user
+        const updatedUser = await UserModel.findByIdAndUpdate(id, { $set: { profilepicture } }, { new: true });
+
+        res.json(updatedUser);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 app.listen(3001, () =>{ // Start on this port
     console.log("server is running")
