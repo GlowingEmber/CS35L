6b7541a62bb477f8d5120fed7e2e4d5e1a12c5b3
Ethan Hopkins <ethan.e.hopkins@gmail.com>
2023-12-05 21:02:37 -0800

    updated friend request model to include defaults

--- server/models/friendreq.js
+++ server/models/friendreq.js
@@ -3,8 +3,14 @@ const mongoose = require('mongoose')
 const friendReqSchema = new mongoose.Schema({
     friender: String,
     recipient: String,
-    timestamp: String,
-    accepted: Boolean, // TRUE for accepted; FALSE for pending/outgoing. Deleted upon rejection by recipient
+    timestamp: {
+        type: Date,
+        default: Date.now
+    },
+    accepted: {
+        type: Boolean, // TRUE for accepted; FALSE for pending/outgoing. Deleted upon rejection by recipient
+        default: false
+    }
 })
 
 const FriendReqModel = mongoose.model("friendreqs", friendReqSchema) // logins is name of table in DB
